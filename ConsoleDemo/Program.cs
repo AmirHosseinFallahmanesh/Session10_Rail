491056a57d0f2275c798c5cdf5428585caec575c
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2022-01-22 11:07:05 +0330

    Add Console

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    interface ISender
+    {
+        void Send(string text);
+    }
+    class MCIGsm : ISender
+    {
+
+
+        public void Send(string text)
+        {
+            Console.WriteLine("Send By MCI");
+        }
+    }
+
+    interface IMark
+    {
+        void Send();
+        int MyProperty { get; set; }
+    }
+}
